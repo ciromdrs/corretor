27d9a5dbfdb0983beba522ed1308362e8632eaa6
Ciro <ciromoraismedeiros@gmail.com>
2024-03-24 16:13:14 -0300

    Criar fixture para testar Atividade

--- test/test_corretor.py
+++ test/test_corretor.py
@@ -1,18 +1,27 @@
-import json, unittest
+import json, pytest, unittest
 
 from src.corretor.corretor import Atividade, Questao, Correcao
 
 from . import TEST_DIR
 
-class TestLerConfig(unittest.TestCase):
-    def setUp(self):
-        arq_config = open(f'{TEST_DIR}/data/config.json')
-        config = json.load(arq_config)
-        arq_config.close()
-        self.atividade = Atividade.ler_config(config)
 
-    def test_atividade_ler_config(self):
+# FIXTURES
+
+@pytest.fixture
+def fxt_atividade():
+    arq_config = open(f'{TEST_DIR}/data/config.json')
+    config = json.load(arq_config)
+    arq_config.close()
+    atividade = Atividade.ler_config(config)
+    return atividade
+
+
+# TESTS
+
+class TestLerConfig:
+
+    def test_atividade_ler_config(self, fxt_atividade):
         '''Testa Atividade.ler_config.'''
-        assert self.atividade.titulo == "Atividade 1"
-        assert len(self.atividade.questoes) == 3
-
+        atividade = fxt_atividade
+        assert atividade.titulo == "Atividade 1"
+        assert len(atividade.questoes) == 3
